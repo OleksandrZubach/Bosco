48f945d608d9f50520a0bd87c69c6a671be84342
admin <sasha.zubovih123@gmail.com>
2024-02-09 23:11:14 +0200

    Готове дз

--- main.py
+++ main.py
@@ -1,119 +1,84 @@
-# def get_user_input():
-#     while True:
-#         name = input('Enter name --> ')
-#         if len(name) > 0 and len(name) <= 16:
-#             print('good')
-#             break
-#         else:
-#             print('bad')
-#     # while True:
-#     moon = int(input('Enter moon --> '))
-#         # if moon > 0 and moon <= 12:
-#         #     print('very good')
-#         #     break
-#         # else:
-#         #     print('very bad')
-#     while True:
-#         year = int(input('Enter year --> '))
-#         if year >= 5 and year <= 120:
-#             print('amazing')
-#             break
-#         else:
-#             print('oops')
-#     return name, moon, year
-#
-# def valide_month():
-#     moon = moon
-#     print(moon)
-#     try:
-#         if moon >= 0 and moon <= 12:
-#             print('very good')
-#         else:
-#             print('very bad')
-#     except ValueError:
-#         print('Невірне значення')
-#
-#
-#
-#
-#
-# print('fsdfsdfsd')
-#
-# if __name__ == '__main__':
-#     get_user_input()
-#     valide_month()
-
-
+import sys
+def main():
+    while True:
+        print('1)Додати користувача 2)Вивести список користувачів 3)Закінчити програму')
+        user_vubir = int(input('Введіть число'))
+        if user_vubir == 1:
+            name, year, month = get_user_input()
+            text_file(name, year, month)
+        elif user_vubir == 2:
+            read_file()
+        elif user_vubir == 3:
+            def clear_file():
+                with open("test_file.txt", "r+") as file:
+                    file.truncate(0)
+                    file.seek(0)
+            print('Дякую що провели час з нами')
+            clear_file()
+            sys.exit()
 
 def get_user_input():
     while True:
         name = input("Введіть ім'я: ")
-        if valide_name(name):
+        if validate_name(name):
             break
         else:
-            # print("Некоректне ім'я. Будь ласка, спробуйте щераз")
-            continue
+            print("Некоректне ім'я. Будь ласка, спробуйте ще раз.")
     while True:
+        global year
         year = int(input("Введіть рік народження (від 5 до 120 років): "))
         if validate_year(year):
             break
         else:
             print("Некоректний рік. Будь ласка, спробуйте ще раз.")
-
     while True:
+        global month
         month = int(input("Введіть місяць народження (від 1 до 12): "))
         if validate_month(month):
             break
         else:
             print("Некоректний місяць. Будь ласка, спробуйте ще раз.")
-
     return name, year, month
 
-def valide_name(name):
-    while True:
-        name = name
-        if 0 < len(name) <=16:
-            break
-        else:
-            print('Невірне значення')
+def validate_name(name):
+    if 0 < len(name) < 16:
+        return name
+    else:
+        print("Погане ім'я")
 
 def validate_year(year):
     try:
-        while True:
-            year = int(year)
-            if 5 <= year <= 120:
-                return year
-            else:
-                print('Введено погане значення')
+        year = int(year)
+        if 5 <= year <= 120:
+            return year
+        else:
+            print("Рік повинен бути в межах від 5 до 120. Будь ласка, спробуйте ще раз.")
     except ValueError:
-        print("Введено не число")
+        print("Введено не число. Будь ласка, спробуйте ще раз.")
 
 def validate_month(month):
     try:
-        while True:
-            month = int(month)
-            if 1 <= month <= 12:
-                return  month
-            else:
-                print('Введено невірне значення')
+        month = int(month)
+        if 1 <= month <= 12:
+            return  month
+        else:
+            print('Введено невірне значення')
     except ValueError:
         print("Введено не число")
 
-
-
-
-def create_test_file(name, year, month):
-    with open("test_file.txt", "w") as file:
-        file.write(name + "\n")
+def text_file(name, year, month):
+    # text_file(name, year, month)
+    with open("test_file.txt", "a") as file:
+        file.write("\n"+name + "\n")
         file.write(str(year) + "\n")
         file.write(str(month) + "\n")
-
-
-def main():
-    name, year, month = get_user_input()
-    create_test_file(name, year, month)
     print("Дані успішно записані у файл 'test_file.txt'.")
 
+def read_file():
+    with open("test_file.txt", "r") as file:
+        read = file.read()
+        print(read)
+        # file.truncate(0)
 
 if __name__ == "__main__":
     main()
--- main2.py
+++ main2.py
@@ -1,1 +1,12 @@
-print('Hi')
+def k():
+    while True:
+        name = input('enter name')
+        if len(name) < 3:
+            print('kros')
+            break
+        # else:
+        #     print('huynya')
+        #     print(len(name))
+
+if __name__ == '__main__':
+    k()
