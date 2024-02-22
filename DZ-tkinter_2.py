017ccedce10f610aa8724d6c6e3356bf88b2b8a7
admin <sasha.zubovih123@gmail.com>
2024-02-22 23:11:23 +0200

    Привіт з дому

--- DZ-tkinter_2.py
+++ DZ-tkinter_2.py
@@ -6,25 +6,24 @@ from tkinter import *
 
 root = Tk()
 
-w = root.winfo_screenwidth()//2-270
-h = root.winfo_screenheight()//2-270
-print(h,w)
-root.geometry(f'400x450+{w}+{h}')
+def quite():
+    root.quit()
 
-root.title('КНП')
-root['bg']='black'
-
-root.resizable(width=False , height=False)
-root.attributes('-topmost',True)
 
 def gra():
     gra = Tk()
 
-    gra.geometry(f'400x450+{w}+{h}')
+    gra.geometry(f'410x450+{w}+{h}')
     gra.title('Гра')
     gra['bg'] = 'black'
     gra.resizable(width=False, height=False)
     gra.attributes('-topmost', True)
+    frame = tk.Frame(gra, width=10, height=5, bg='lightblue')
+    frame.pack(padx=10, pady=250)
+    g = root.winfo_screenwidth()
+    q = root.winfo_screenheight()
+    print(g,q)
+
 
     def vash_vubir(choise):
 
@@ -50,21 +49,32 @@ def gra():
 
 
 
-    # gra.mainloop()
+w = root.winfo_screenwidth()//2-270
+h = root.winfo_screenheight()//2-270
+print(h,w)
+root.geometry(f'400x450+{w}+{h}')
+
+root.title('КНП')
+root['bg']='forestgreen'
+
+root.resizable(width=False , height=False)
+root.attributes('-topmost',True)
+
+frame_home = tk.Frame(root, width=300, height=400, bg='grey' , relief='ridge' , bd = 3)
+frame_home.pack(padx=10, pady=15)
 
 
+label1 = Label(root,text='КАМІНЬ - НОЖНИЦІ - ПАПІР', bg='yellow', fg='black', font=('Comic Sams MS',15) , pady=7,padx=15, relief='ridge' , bd=3 )
+label1.place(x = 50, y = 15)
 
-label1 = Label(root,text='КАМІНЬ - НОЖНИЦІ - ПАПІР', bg = 'black' , fg='white', font=('Comic Sams MS',15), relief='ridge',pady=5,padx=5 , bd=3  )
-label1.place(x = 60, y = 15)
+but1 = Button(root, text='Грати' , bg= 'yellow', fg='black', padx=55 , pady=10 , bd=3 , font=('Comic Sans MS',15), relief='ridge', activebackground='lime' ,command=gra)
+but1.place(x = 110 , y = 100)
 
-but1 = Button(root, text='ГРАТИ' , bg= 'black', fg='white', padx=15 , pady=10 , bd=3 , font=('Comic Sans MS',15), relief='ridge' ,command=gra)
-but1.place(x = 150 , y = 100)
+but2 = Button(root, text='Рахунок' , bg= 'yellow', fg='black', padx=45 , pady=10 , bd=3 , font=('Comic Sans MS',15), relief='ridge', activebackground='lime')
+but2.place(x = 110 , y = 200)
 
-but2 = Button(root, text='Рахунок' , bg= 'black', fg='white', padx=10 , pady=10 , bd=3 , font=('Comic Sans MS',15), relief='ridge')
-but2.place(x = 150 , y = 200)
-
-but3 = Button(root, text='Вихід' , bg= 'black', fg='white', padx=20 , pady=10 , bd=3 , font=('Comic Sans MS',15), relief='ridge')
-but3.place(x = 150 , y = 300)
+but3 = Button(root, text='Вихід' , bg= 'yellow', fg='black', padx=55 , pady=10 , bd=3 , font=('Comic Sans MS',15), relief='ridge',activebackground='lime', command=quit)
+but3.place(x = 110 , y = 300)
 
 
 root.mainloop()
